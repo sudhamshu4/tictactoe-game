77aaea8c8fee1ba9c5044cfa82892a1bfeb83264
SUD <you@sudhamshumulky@gmail.com>
2023-03-10 09:35:11 +0530

    introduced a player.when clicked on a square show either a X or o

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import { useState } from 'react';
 import './styles.scss';
+import Board from './components/Board';
 
 function App() {
-  const [counter, setCounter] = useState(1);
-
-  console.log('hello');
-
-  const onBtnClick = () => {
-    setCounter(currentCounter => {
-      return currentCounter + 1;
-    });
-  };
-
   return (
     <div className="app">
-      <button onClick={onBtnClick}>Click me please</button>
-      <div>{counter}</div>
+      <Board />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,4 @@ import App from './App';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
